26b281157863e75b73fcd84f5201dda3d40fe7a3
Gabsplat <gaabgames@gmail.com>
2023-10-11 23:38:47 -0300

    Masonry started, weird though. Navbar sticky

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -5,9 +5,11 @@ export default function Logo() {
   return (
     <Link
       href="/"
-      className="text-2xl lg:text-4xl text-imagine-brown font-bold"
+      className="text-2xl lg:text-4xl text-imagine-brown font-semibold"
     >
-      Imagine
+      <span className="font-bold">Imag</span>
+      <span className="text-imagine-brown/60">in</span>
+      <span className="font-bold">e</span>
     </Link>
   );
 }
